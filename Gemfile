253b7bae54419e280ab135d671130f557eb85d37
kolan4ick <Kolan4ick@gmail.com>
2024-04-15 22:27:07 +0300

    Add initial simple logic

--- Gemfile
+++ Gemfile
@@ -8,3 +8,9 @@ gemspec
 gem "rake", "~> 13.0"
 
 gem "minitest", "~> 5.16"
+
+gem "uri", "~> 0.10"
+
+gem 'net-http', '~> 0.1.1'
+
+gem 'json', '~> 2.5'
--- lib/microsoft_translator.rb
+++ lib/microsoft_translator.rb
@@ -1,8 +1,34 @@
 # frozen_string_literal: true
 
 require_relative "microsoft_translator/version"
+require 'net/http'
+require 'json'
 
 module MicrosoftTranslator
   class Error < StandardError; end
-  # Your code goes here...
+
+  class TranslationApi
+    def initialize(subscription_key, region)
+      @subscription_key = subscription_key
+      @region = region
+    end
+
+    def translate(text, to, from = nil)
+      uri = URI("https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=#{to}")
+      uri.query = URI.encode_www_form({ "api-version" => "3.0", "to" => to })
+      request = Net::HTTP::Post.new(uri)
+      request["Ocp-Apim-Subscription-Key"] = @subscription_key
+      request["Ocp-Apim-Subscription-Region"] = @region
+      request["Content-type"] = "application/json"
+      request["Content-length"] = text.length
+      request.body = [{ "Text" => text }].to_json
+
+      response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do | http |
+        http.request(request)
+      end
+
+      # Get only the first translation
+      JSON.parse(response.body)[0]["translations"][0]["text"]
+    end
+  end
 end
